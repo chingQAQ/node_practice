d8bf44c9069741db4e806f6788349327c3ed8546
Aion Chiu <aion.chiu@isobar.com>
2021-01-06 14:02:42 +0800

    feat: 新增功能

--- src/apis/api.js
+++ src/apis/api.js
@@ -5,18 +5,25 @@ export const HOST = 'https://www.dcard.tw';
 const API_ENDPOINT = 'https://www.dcard.tw/service/api/v2';
 
 export async function api(path, { query, headers } = {}) {
-  // const endpoint = '/' + path + '?popular=true';
-  const url = `${API_ENDPOINT}/${path}?popular=true`
-  console.log(querystring.stringify({popular: true}));
+  const filtered = {};
+  for (const i of Object.keys(query)) {
+    if (query[i] !== null) {
+      filtered[i] = query[i];
+    }
+  }
+
+  const endpoint = `${API_ENDPOINT}/${path}${
+    Object.keys(filtered).length > 0 ? `?${
+      querystring.stringify(filtered)}`: ''}
+  `
   try {
-    const result = await fetch(url, { headers });
-    const data = result.json();
-
+    const result = await fetch(endpoint, { headers });
+    const data = await result.json();
+    
     if (data.error) throw data;
-
-    return data;
+    if (data.length) return data;
+    
   } catch(err) {
     console.log(err);
   }
 }
-
---
+++ src/app.js
@@ -0,0 +1,123 @@
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+
+import express from 'express';
+import path from 'path';
+import { api } from './apis';
+
+import * as line from '@line/bot-sdk';
+
+const LINE_SDK_CONFIG = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+}
+const PORT = process.env.BASE_PORT || '3000';
+
+const app = express();
+const client = new line.Client(LINE_SDK_CONFIG);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/tracking', (req, res) => {
+  res.render('tracking')
+})
+
+// 1. user 發送訊息
+app.post('/callback/', line.middleware(LINE_SDK_CONFIG), (req, res) => {
+  // 3. 傳送 request
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => {
+      return res.json(result)
+    })
+    .catch(err => console.log(err));
+})
+
+async function handleEvent(event) {
+
+  let message = [];
+
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  // 4. response
+  if (event.type === 'message' || event.message.type === 'text') {
+    
+    if (event.message.text === 'posts') {
+      const fetchDCard = await api(event.message.text, { query: {'popular': true}});
+      message = fetchDCard.slice(0, 12);
+    }
+    
+  }
+
+  message = message.map(i => {
+    const hero = i.mediaMeta[0] && { 
+      "type": "image",
+      "url": i.mediaMeta[0].url,
+      "size": "full",
+      "aspectMode": "cover",
+      "aspectRatio": "320:213"
+    };
+
+    return {
+      "type": "bubble",
+      "size": "kilo",
+      hero,
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": i.title || '',
+            "weight": "bold",
+            "size": "sm",
+            "wrap": true
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": i.excerpt || '',
+                    "wrap": true,
+                    "color": "#8c8c8c",
+                    "size": "xs",
+                    "flex": 5
+                  }
+                ]
+              }
+            ]
+          }
+        ],
+        "spacing": "sm",
+        "paddingAll": "13px"
+      }
+    }
+  })
+
+  console.log(message);
+
+  // 5. 回傳資料
+  return client.replyMessage(event.replyToken, {
+    "type": "flex",
+    "altText": "this is a flex message",
+    "contents": {
+      "type": "carousel",
+      "contents": message
+    }
+  });
+}
+
+app.listen(PORT);
