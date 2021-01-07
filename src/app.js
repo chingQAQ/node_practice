d86b15ac7534ff20a5f9d67d48e9086af1b2c4d3
Aion Chiu <aion.chiu@isobar.com>
2021-01-07 19:20:57 +0800

    feat: 增加rich menu

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import path from 'path';
 import querystring from 'querystring';
 import { HOST, api } from './apis';
 import { flexHero, flexStructure } from './util';
+import fs from 'fs';
 
 import * as line from '@line/bot-sdk';
 
@@ -21,12 +22,75 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index');
 })
 
 app.get('/tracking', (req, res) => {
-  res.render('tracking')
+  res.render('tracking');
 })
+
+const richmenu = {
+  size: {
+    width: 1200,
+    height: 810
+  },
+  selected: false,
+  name: 'Laibb Rich!',
+  chatBarText: 'Laibb tap',
+  areas: [
+    {
+      bounds: {
+        x: 0,
+        y: 0,
+        width: 400,
+        height: 810,
+      },
+      action: {
+        type: "postback",
+        label: "Tool",
+        data: "action=tool",
+      },
+    },
+    {
+      bounds: {
+        x: 400,
+        y: 0,
+        width: 400,
+        height: 810,
+      },
+      action: {
+        type: "postback",
+        label: "Tool",
+        data: "action=none",
+      },
+    },
+    {
+      bounds: {
+        x: 800,
+        y: 0,
+        width: 400,
+        height: 810,
+      },
+      action: {
+        type: "postback",
+        label: "Tool",
+        data: "action=nextRich",
+      },
+    }
+  ]
+}
+
+// client.createRichMenu(richmenu).then(result => console.log(result)).catch(err => console.log(err));
+
+client
+  .getRichMenuList()
+  .then(async richMenu => {
+    richMenu[0].richMenuId && await client.setRichMenuImage(richMenu[0].richMenuId, fs.createReadStream(path.join(__dirname, 'public', 'rich_menu.jpg')));
+    await client.setDefaultRichMenu(richMenu[0].richMenuId);
+    return console.log('done');
+  })
+
+
 
 // 1. user 發送訊息
 app.post('/callback/', line.middleware(LINE_SDK_CONFIG), (req, res) => {
@@ -62,33 +126,22 @@ async function handleEvent(event) {
           "contents": message
         }
       });
-    } else {
+    } else if (data.action === 'nextRich') {
       return client.replyMessage(event.replyToken, {
         "type": "text",
-        "text": "$ 找不到指令喔 $",
+        "text": "$ 預計是下一頁Rich menu呀",
         "emojis": [
           {
             "index": 0,
             "productId": "5ac1bfd5040ab15980c9b435",
-            "emojiId": "164"
+            "emojiId": "163"
           },
-          {
-            "index": 9,
-            "productId": "5ac1bfd5040ab15980c9b435",
-            "emojiId": "164"
-          }
         ]
-      });
-    }
-  }
-
-  // 4. response
-  if (event.type === 'message' || event.message.type === 'text') {
-
-    if (event.message.text === '!功能') {
+      })
+    } else if (data.action === 'tool') {
       return client.replyMessage(event.replyToken, {
         "type": 'text',
-        "text": '我還在測試拉顆顆顆不要急喔(Quick reply)',
+        "text": '我還在測試拉顆顆顆(Quick reply)',
         "quickReply": {
           "items": [
             {
@@ -115,6 +168,23 @@ async function handleEvent(event) {
             }
           ]
         }
+      });
+    } else {
+      return client.replyMessage(event.replyToken, {
+        "type": "text",
+        "text": "$ 找不到指令喔 $",
+        "emojis": [
+          {
+            "index": 0,
+            "productId": "5ac1bfd5040ab15980c9b435",
+            "emojiId": "164"
+          },
+          {
+            "index": 9,
+            "productId": "5ac1bfd5040ab15980c9b435",
+            "emojiId": "164"
+          }
+        ]
       });
     }
   }
